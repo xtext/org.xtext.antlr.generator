70a0cc267533613b5d907ca26764bbb1fdc4ca0f
sefftinge <sefftinge>
2009-02-18 19:37:56 +0000

    - modularization of generator (use of MWE) - IMetaModelAccess removed - Integration of EMF's Ecore Generator

---
+++ de.itemis.xtext.antlr/src/de/itemis/xtext/antlr/XtextAntlrGeneratorFragment.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package de.itemis.xtext.antlr;
+
+import static org.eclipse.xtext.util.Tuples.*;
+
+import java.util.Map;
+
+import org.eclipse.xpand2.XpandExecutionContext;
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+import org.eclipse.xtext.generator.Generator;
+import org.eclipse.xtext.parser.ITokenToStringConverter;
+import org.eclipse.xtext.parser.antlr.AntlrTokenDefProvider;
+import org.eclipse.xtext.parser.antlr.AntlrTokenToStringConverter;
+import org.eclipse.xtext.parser.antlr.IAntlrParser;
+import org.eclipse.xtext.parser.antlr.IAntlrTokenFileProvider;
+import org.eclipse.xtext.parser.antlr.ITokenDefProvider;
+import org.eclipse.xtext.parser.antlr.Lexer;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ * @author Sven Efftinge
+ */
+public class XtextAntlrGeneratorFragment extends AbstractGeneratorFragment {
+	
+	@Override
+	public void generate(Grammar grammar, XpandExecutionContext ctx) {
+		super.generate(grammar, ctx);
+		String srcGenPath = ctx.getOutput().getOutlet(Generator.SRC_GEN).getPath();
+		de.itemis.xtext.antlr.AntlrToolRunner.run(srcGenPath+"/"+getGrammarFileName(grammar).replace('.', '/')+".g");
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map<String, String> getGuiceBindingsRt(Grammar grammar) {
+		return toMap(pair(IAntlrParser.class.getName(),getParserClassName(grammar)),
+					pair(ITokenToStringConverter.class.getName(),AntlrTokenToStringConverter.class.getName()),
+					pair(IAntlrTokenFileProvider.class.getName(),getAntlrTokenFileProviderClassName(grammar)),
+					pair(Lexer.class.getName(),getLexerClassName(grammar)),
+					pair(ITokenDefProvider.class.getName(),AntlrTokenDefProvider.class.getName()));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map<String, String> getGuiceBindingsUi(Grammar grammar) {
+		return toMap(pair("org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.ITokenColorer","org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.BuiltinAntlrTokenColorer"),
+					pair("org.eclipse.jface.text.rules.ITokenScanner","org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.AntlrTokenScanner"));
+	}
+
+	public static String getAntlrTokenFileProviderClassName(Grammar grammar) {
+		return GrammarUtil.getNamespace(grammar) + ".parser.antlr." + GrammarUtil.getName(grammar)	+ "AntlrTokenFileProvider";
+	}
+
+	public static String getLexerClassName(Grammar g) {
+		return GrammarUtil.getNamespace(g) + ".parser.antlr.internal.Internal" + GrammarUtil.getName(g)	+ "Lexer";
+	}
+
+	public static String getParserClassName(Grammar g) {
+		return GrammarUtil.getNamespace(g) + ".parser.antlr." + GrammarUtil.getName(g) + "Parser";
+	}
+	
+	public static String getInternalParserClassName(Grammar g) {
+		return GrammarUtil.getNamespace(g) + ".parser.antlr.internal.Internal" + GrammarUtil.getName(g) + "Parser";
+	}
+	
+	public static String getGrammarFileName(Grammar g) {
+		return GrammarUtil.getNamespace(g) + ".parser.antlr.internal.Internal" + GrammarUtil.getName(g);
+	}
+	
+}
