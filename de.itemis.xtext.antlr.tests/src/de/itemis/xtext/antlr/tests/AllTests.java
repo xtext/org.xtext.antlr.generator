3e837dd21026f313e58dd147250475f23acca573
d_huebner <d_huebner>
2009-11-05 14:12:09 +0000

    setup running tests during the build

---
+++ de.itemis.xtext.antlr.tests/src/de/itemis/xtext/antlr/tests/AllTests.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package de.itemis.xtext.antlr.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import de.itemis.xtext.antlr.AntlrGrammarGenUtilTest;
+import de.itemis.xtext.antlr.TerminalRuleToLexerBodyTest;
+
+/**
+ * @author huebner - Initial contribution and API
+ */
+public class AllTests {
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Tests for org.eclipse.emf.emfindex.tests");
+		suite.addTestSuite(AntlrGrammarGenUtilTest.class);
+		suite.addTestSuite(TerminalRuleToLexerBodyTest.class);
+		return suite;
+	}
+}
