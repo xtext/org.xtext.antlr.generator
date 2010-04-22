dd62d3252388e566a8b1f1fd91f71b134cb38857
docfx <docfx>
2010-04-22 15:50:00 +0000

    Addition to bug 304240: Adapted the UnorderedGroupsSplitter

--- de.itemis.xtext.antlr/src/de/itemis/xtext/antlr/splitting/UnorderedGroupsSplitter.java
+++ de.itemis.xtext.antlr/src/de/itemis/xtext/antlr/splitting/UnorderedGroupsSplitter.java
@@ -16,15 +16,16 @@ import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Collections;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.parser.IDefaultEncodingProvider;
 import org.eclipse.xtext.parsetree.reconstr.SerializerUtil;
 import org.eclipse.xtext.resource.IResourceFactory;
 import org.eclipse.xtext.resource.XtextResource;
 
 import com.google.inject.Injector;
+import com.google.inject.internal.Maps;
 
 import de.itemis.xtext.antlr.splitting.simpleExpressions.IfCondition;
 
@@ -33,6 +34,7 @@ import de.itemis.xtext.antlr.splitting.simpleExpressions.IfCondition;
  */
 public class UnorderedGroupsSplitter {
 
+	private final static String INTERNAL_ENCODING = "UTF-8";
 	private final String content;
 	private ConditionSimplifier simplifier;
 
@@ -44,12 +46,10 @@ public class UnorderedGroupsSplitter {
 	public String transform() {
 		Injector injector = new SimpleExpressionsStandaloneSetup().createInjectorAndDoEMFRegistration();
 		IResourceFactory resourceFactory = injector.getInstance(IResourceFactory.class);
-		IDefaultEncodingProvider encodingProvider = injector.getInstance(IDefaultEncodingProvider.class);
-		String encoding = encodingProvider.getEncoding();
-		return transformContent(resourceFactory, encoding); 
+		return transformContent(resourceFactory); 
 	}
 
-	protected String transformContent(IResourceFactory resourceFactory, String encoding) {
+	protected String transformContent(IResourceFactory resourceFactory) {
 		BufferedReader reader = new BufferedReader(new StringReader(content));
 		try {
 			try {
@@ -57,7 +57,7 @@ public class UnorderedGroupsSplitter {
 				PrintWriter printer = new PrintWriter(writer);
 				String line = reader.readLine();
 				while(line != null) {
-					line = transfromLine(line, resourceFactory, encoding);
+					line = transfromLine(line, resourceFactory);
 					printer.println(line);
 					line = reader.readLine();
 				}
@@ -71,14 +71,14 @@ public class UnorderedGroupsSplitter {
 		}
 	}
 	
-	public String transfromLine(String line, IResourceFactory resourceFactory, String encoding) throws IOException {
+	public String transfromLine(String line, IResourceFactory resourceFactory) throws IOException {
 		if (shouldSimplify(line)) {
 			int braceIdx = line.indexOf('{');
 			String lineAsInput = line.substring(0, braceIdx + 1);
-			Resource resource = getResource(lineAsInput, resourceFactory, encoding);
+			Resource resource = getResource(lineAsInput, resourceFactory);
 			IfCondition condition = (IfCondition) resource.getContents().get(0);
 			simplifier.simplify(condition);
-			String fixedLine = saveResource(encoding, resource);
+			String fixedLine = saveResource(resource);
 			fixedLine = addPreAndPostfix(line, braceIdx, fixedLine);
 			return fixedLine;
 		} else {
@@ -101,22 +101,25 @@ public class UnorderedGroupsSplitter {
 		return fixedLine;
 	}
 
-	protected String saveResource(String encoding, Resource resource) throws IOException {
+	protected String saveResource(Resource resource) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream(content.length());
 		try {
-			resource.save(out, Collections.singletonMap(XtextResource.OPTION_SERIALIZATION_OPTIONS, SerializerUtil.NO_FORMAT));
-			String result = new String(out.toByteArray(), encoding);
+			Map<String, Object> options = Maps.newHashMap();
+			options.put(XtextResource.OPTION_ENCODING, INTERNAL_ENCODING);
+			options.put(XtextResource.OPTION_SERIALIZATION_OPTIONS, SerializerUtil.NO_FORMAT);
+			resource.save(out, options);
+			String result = new String(out.toByteArray(), INTERNAL_ENCODING);
 			return result;
 		} finally {
 			out.close();
 		}
 	}
 
-	protected Resource getResource(String input, IResourceFactory resourceFactory, String encoding)	throws IOException {
-		InputStream inputStream = new ByteArrayInputStream(input.getBytes(encoding));
+	protected Resource getResource(String input, IResourceFactory resourceFactory) throws IOException {
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes(INTERNAL_ENCODING));
 		try {
 			Resource resource = resourceFactory.createResource(URI.createURI("dummy.simpleexpressions"));
-			resource.load(inputStream, null);
+			resource.load(inputStream, Collections.singletonMap(XtextResource.OPTION_ENCODING, INTERNAL_ENCODING));
 			if (!resource.getErrors().isEmpty()) {
 				throw new RuntimeException(input + " - " + resource.getErrors().toString());
 			}
