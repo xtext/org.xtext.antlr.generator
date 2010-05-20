d46ec6082c8ab1f43ec9c1f1f36b3cf3fd306cf7
sefftinge <sefftinge>
2010-05-20 17:33:37 +0000

    restructured antlr generator (https://bugs.eclipse.org/bugs/show_bug.cgi?id=313663)

--- de.itemis.xtext.antlr.tests/src/de/itemis/xtext/antlr/tests/AllTests.java
+++ de.itemis.xtext.antlr.tests/src/de/itemis/xtext/antlr/tests/AllTests.java
@@ -9,12 +9,6 @@ package de.itemis.xtext.antlr.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import de.itemis.xtext.antlr.AntlrGrammarGenUtilTest;
-import de.itemis.xtext.antlr.TerminalRuleToLexerBodyTest;
-import de.itemis.xtext.antlr.ex.common.KeywordHelperTest;
-import de.itemis.xtext.antlr.splitting.AntlrLexerSplitterTest;
-import de.itemis.xtext.antlr.splitting.AntlrParserSplitterTest;
-import de.itemis.xtext.antlr.splitting.CondititionSimplifierTest;
 
 /**
  * @author huebner - Initial contribution and API
@@ -22,12 +16,6 @@ import de.itemis.xtext.antlr.splitting.CondititionSimplifierTest;
 public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Tests for de.itemis.xtext.antlr.tests");
-		suite.addTestSuite(AntlrGrammarGenUtilTest.class);
-		suite.addTestSuite(TerminalRuleToLexerBodyTest.class);
-		suite.addTestSuite(KeywordHelperTest.class);
-		suite.addTestSuite(AntlrLexerSplitterTest.class);
-		suite.addTestSuite(AntlrParserSplitterTest.class);
-		suite.addTestSuite(CondititionSimplifierTest.class);
 		return suite;
 	}
 }
