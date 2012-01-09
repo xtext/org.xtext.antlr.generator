ed885bb91013a98282b4fbc3c8f1f9ce2e0e68f7
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-01-09 14:22:21 +0100

    [xtext-antlr][tests] Migrated to JUnit 4.x

--- de.itemis.xtext.antlr.tests/src/de/itemis/xtext/antlr/tests/Dummy.java
+++ de.itemis.xtext.antlr.tests/src/de/itemis/xtext/antlr/tests/Dummy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,15 +7,17 @@
  *******************************************************************************/
 package de.itemis.xtext.antlr.tests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
- * @author huebner - Initial contribution and API
+ * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class AllTests {
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Tests for de.itemis.xtext.antlr.tests");
-		return suite;
+public class Dummy {
+
+	@Test
+	public void dummy() {
+		Assert.assertTrue(true);
 	}
+	
 }
